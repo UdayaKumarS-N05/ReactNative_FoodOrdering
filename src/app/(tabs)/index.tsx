d5ea1ff8edad54d059e4442c2d44573259689cba
Udaya Kumar S <udaydavis144@gmail.com>
2024-08-20 23:29:23 +0530

    test

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text, View } from "@/src/components/Themed";
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
+      <Text style={styles.title}>Hello New Text</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
